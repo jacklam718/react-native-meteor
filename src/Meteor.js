cd45683193b6221ee51d65d3f7bfb5805129f94f
Jack Lam <jacklam718@gmail.com>
2016-08-05 19:20:00 +0800

    add callPromise method in Meteor.js

--- src/Meteor.js
+++ src/Meteor.js
@@ -55,6 +55,23 @@ module.exports = {
     }
   },
   call: call,
+  callPromise(methodName, params) {
+    params = params || undefined;
+    // if (params && !_.isArray(params)) {
+    //   console.warn('Params must be passed as an array to ddp.call');
+    // }
+
+    return new Promise((resolve, reject) => {
+      call(methodName, params, (err, result) => {
+          // callback which returns the method call results
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
   disconnect() {
     if(Data.ddp) {
       Data.ddp.disconnect();
